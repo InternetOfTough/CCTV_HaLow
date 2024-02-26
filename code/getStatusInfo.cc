1b38bcee2f8b7ed8bd62381744fe22d49b942170
MinjooShin <als000313@naver.com>
2024-02-26 16:57:47 +0900

    feat: 카메라 상태 확인 기능 적용

--- code/getStatusInfo.cc
+++ code/getStatusInfo.cc
@@ -6,34 +6,49 @@ using namespace std;
 
 void runSignalLevel();
 void runNetworkTraffic();
+void runCamera();
 
 int main() {
     while (true) {
-        // sinalLevel.cpp 실행
-        cout << "sinalLevel.cpp 실행 중..." << endl;
+        // sinalLevel.cc 실행
+        cout << "sinalLevel.cc 실행 중..." << endl;
         thread t1(runSignalLevel);
+        // 10초 대기
+        this_thread::sleep_for(chrono::seconds(10));
 
-        // networkTraffic.cpp 실행
-        cout << "networkTraffic.cpp 실행 중..." << endl;
+        // networkTraffic.cc 실행
+        cout << "networkTraffic.cc 실행 중..." << endl;
         thread t2(runNetworkTraffic);
+        // 10초 대기
+        this_thread::sleep_for(chrono::seconds(10));
+
+        // camera.cc 실행
+        cout << "camera.cc 실행 중..." << endl;
+        thread t3(runCamera);
+        // 60초 대기
+        this_thread::sleep_for(chrono::seconds(60));
 
         // 스레드 종료 대기
         t1.join();
         t2.join();
-
-        // 3초 대기
-        this_thread::sleep_for(chrono::seconds(3));
+        t3.join();
+        
     }
 
     return 0;
 }
 
-// sinalLevel.cpp 실행 함수
+// sinalLevel.cc 실행 함수
 void runSignalLevel() {
     system("./sinalLevel");
 }
 
-// networkTraffic.cpp 실행 함수
+// networkTraffic.cc 실행 함수
 void runNetworkTraffic() {
     system("./networkTraffic");
+}
+
+// camera.cc 실행 함수
+void runCamera() {
+    system("./camera");
 }
